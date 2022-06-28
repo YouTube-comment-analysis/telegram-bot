d689d5e36025f2909f40377fc0a20a13a17dfb20
Maxim <machsim26@gmail.com>
2022-06-28 22:26:58 +0500

    получение всех комментариев без ответа

--- youtube_parser.py
+++ youtube_parser.py
@@ -5,10 +5,9 @@ api = Api(api_key=config.youtube_dev_api_key)
 
 
 async def scrap_comments(video_id):
-    comments = api.get_comment_threads(parts=['snippet', 'replies'], video_id=video_id, count=9999999999)
+    comments = await api.get_comment_threads(parts=['snippet', 'replies'], video_id=video_id, count=9999999999)
     arr = []
     for i in comments.items:
         comm = i.snippet.topLevelComment.snippet.textDisplay
         arr.append(comm)
-        print(comm + '\n')
     return arr
