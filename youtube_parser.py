f6d5650d49f21ac5bd5a28c44128b5425532a9e1
Maxim <machsim26@gmail.com>
2022-06-29 00:14:08 +0500

    text without html tags

--- youtube_parser.py
+++ youtube_parser.py
@@ -1,14 +1,12 @@
-import pyyoutube
-import numpy as np
 from pyyoutube import Api
 import config
 api = Api(api_key=config.youtube_dev_api_key)
 
 
 def scrap_comments(video_id: str) -> []:
-    comments = api.get_comment_threads(parts=['snippet', 'replies'], video_id=video_id, count=9999999999)
+    comments = api.get_comment_threads(parts=['snippet', 'replies'], video_id=video_id, count=1000)
     arr = []
     for i in comments.items:
-        comm = i.snippet.topLevelComment.snippet.textDisplay
+        comm = i.snippet.topLevelComment.snippet.textOriginal
         arr.append(comm)
     return arr
