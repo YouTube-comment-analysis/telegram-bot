6ca6a3d759456dca01fb359b6f65ccdba10c83a5
mestnq <70838438+mestnq@users.noreply.github.com>
2022-07-04 20:28:33 +0500

    Delete config

--- config.py
+++
@@ -1,8 +0,0 @@
-youtube_dev_api_key = 'AIzaSyB5vfYJHV582eSq8zzdx6AmneA6m47_nt4'
-telegram_bot_token = '5572095516:AAETyYlnp4ysE3ERvcfwj8dTsG4MEeT20zY'
-database_config = {'user': "maxim",
-                   'password': "powerpivo",
-                   'host': "maximchuikov.demlovesky.ru",
-                   'port': "49159",
-                   'database': "telegram_bot"
-                   }
