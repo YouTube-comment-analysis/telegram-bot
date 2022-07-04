a4cb86916e7254d62bc05634fb5be50aff0a82dc
Maxim <machsim26@gmail.com>
2022-07-04 22:13:57 +0500

    Добавлены history методы, исправлена замена в файле соединения с бд

--- database_interaction/db_connection.py
+++ database_interaction/db_connection.py
@@ -2,7 +2,7 @@ import psycopg2 as db
 from config import database_config
 
 connection = db.connect(
-    database=database_config['database_interaction'],
+    database=database_config['database'],
     user=database_config['user'],
     password=database_config['password'],
     host=database_config['host'],
--- database_interaction/history.py
+++ database_interaction/history.py
@@ -14,29 +14,56 @@ def get_all_channel_history() -> [ChannelHistory]:
     with connection as connect:
         with connect.cursor() as curs:
             curs.execute("""
-    SELECT
-    user_id, channel_url, viewing_date
-    FROM
-    public.history_channel;
-    ORDER BY viewing_date DESC
-                """,)
-            return map(lambda x: ChannelHistory(x['user_id'], x['channel_url'], x['viewing_date']), curs)
+SELECT
+user_id, channel_url, viewing_date
+FROM
+public.history_channel;
+ORDER BY viewing_date DESC
+                """)
+            return map(lambda x: ChannelHistory(x[0], x[1], x[2]), curs)
 
 
-def get_user_channel_history(user_telegram_id: int, limited: bool = False):
+def get_user_channel_history(user_telegram_id: int, limited: bool = False) -> [ChannelHistory]:
     """Получить список истории по анализу канала у одного пользователя, можно ограничить выборку десятью первыми элементами"""
     with connection as connect:
         with connect.cursor() as curs:
             curs.execute("""
-    SELECT
-    user_id, channel_url, viewing_date
-    FROM
-    public.history_channel
-    WHERE user_id = %s
-    ORDER BY viewing_date DESC
-                """, (user_telegram_id,))
-            result = map(lambda x: ChannelHistory(x['user_id'], x['channel_url'], x['viewing_date']), curs)
-            return result[:10] if limited else result
+SELECT
+user_id, channel_url, viewing_date
+FROM public.history_channel
+WHERE user_id = %s
+ORDER BY viewing_date DESC
+                """ + ' LIMIT 10' if limited else '', (user_telegram_id,))
+            return map(lambda x: ChannelHistory(x[0], x[1], x[2]), curs)
 
 
+class VideoHistory:
+    def __init__(self, user_id: int, url: str, viewing_date: datetime):
+        self.user_id = user_id
+        self.url = url,
+        self.viewing_date = viewing_date
 
+
+def get_all_video_history() -> [ChannelHistory]:
+    """Получить весь список истории по анализу видео у всех пользователей"""
+    with connection as connect:
+        with connect.cursor() as curs:
+            curs.execute("""
+SELECT
+user_id, url, viewing_date
+FROM public.history_video
+                """)
+            return map(lambda x: VideoHistory(x[0], x[1], x[2]), curs)
+
+
+def get_user_video_history(user_telegram_id: int, limited: bool = False) -> [ChannelHistory]:
+    """Получить список истории по анализу видео у одного пользователя, можно ограничить выборку десятью первыми элементами"""
+    with connection as connect:
+        with connect.cursor() as curs:
+            curs.execute("""
+SELECT
+user_id, url, viewing_date
+FROM public.history_video
+WHERE user_id = %s
+                """ + ' LIMIT 10' if limited else '', (user_telegram_id,))
+            return map(lambda x: ChannelHistory(x[0], x[1], x[2]), curs)
