748f39ebac105f3a22fc238dfa007111049eeb3d
Maxim <machsim26@gmail.com>
2022-07-04 12:05:59 +0500

    Добавлена документация и новые методы

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import psycopg2 as db
 from config import database_config
+import enum
 
 connection = db.connect(
     database=database_config['database'],
@@ -10,10 +11,11 @@ connection = db.connect(
 )
 
 
-def extract_comments(video_id: str, ordered: bool):
+def extract_comments(video_id: str, ordered_by_date: bool):
+    """Получение всех сохраненных комментарие по id видео"""
     cursor = connection.cursor()
     result = []
-    if ordered:
+    if ordered_by_date:
         cursor.execute('SELECT text, writing_date, likes, popular_order, is_reply '
                        'FROM public.commentary WHERE '
                        f'url = %s', (video_id,))
@@ -28,6 +30,7 @@ def extract_comments(video_id: str, ordered: bool):
 
 
 def load_comments(video_id: str, comments: []):
+    """Загрузить комментарии по id видео"""
     with connection as connect:
         with connect.cursor() as curs:
             for row in comments:
@@ -38,6 +41,7 @@ def load_comments(video_id: str, comments: []):
 
 
 def insert_video(channel_id: str, video_id: str):
+    """Добавить видео в базу"""
     with connection as connect:
         with connect.cursor() as curs:
             curs.execute('INSERT INTO public.video('
@@ -46,6 +50,7 @@ def insert_video(channel_id: str, video_id: str):
 
 
 def update_scrap_date(video_id: str):
+    """Обновить дату получения комментариев"""
     with connection as connect:
         with connect.cursor() as curs:
             curs.execute('UPDATE public.video '
@@ -54,8 +59,35 @@ def update_scrap_date(video_id: str):
 
 
 def insert_channel(channel_id: str):
+    """Добавить канал в базу"""
     with connection as connect:
         with connect.cursor() as curs:
             curs.execute('INSERT INTO public.channel('
                          'channel_url) '
                          f'VALUES (%s);', (channel_id,))
+
+
+class GlobalSettings(enum.Enum):
+    maximum_number_of_comments_to_download = 'max_comments_from_video'
+
+
+def get_global_setting(setting: GlobalSettings):
+    """Получить значение глобальной настройки"""
+    with connection as connect:
+        with connect.cursor() as curs:
+            curs.execute("""
+SELECT setting, value
+    FROM public.global_settings
+    WHERE setting = %s
+""", (setting.value,))
+
+
+def update_global_setting(setting: GlobalSettings, value: int):
+    """Обновить значение глобальной настройки"""
+    with connection as connect:
+        with connect.cursor() as curs:
+            curs.execute("""
+UPDATE public.global_settings
+    SET value = %s
+    WHERE setting = %s;
+""", (value, setting.value))
