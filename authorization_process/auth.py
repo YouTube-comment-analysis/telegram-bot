0724205dc17f01a63d7d4a7dd9a2042d0f667e28
Maxim <machsim26@gmail.com>
2022-07-06 19:02:14 +0500

    Добавлен словарь авторизованных пользователей

--- authorization_process/auth.py
+++ authorization_process/auth.py
@@ -5,29 +5,48 @@ from authorization_process.password_encryption import is_correct_password, hash_
 user = database.user.UserCabinet
 role = database.user.UserRole
 
+authorized_users = dict()
+
 
 def sign_up(
         user_cabinet: user,
         user_role: role,
         login: str,
         password: str
-        ) -> int:
-    """
-    Регистрация пользователя в базе
-    :return: user_id, уникальный идентификатор пользователя
-    """
+        ):
+    """Регистрация пользователя в базе"""
     user_id = auth.create_user_and_cabinet(user_role, user_cabinet)
     pwd_salt, pwd_hash = hash_new_password(password)
     auth.insert_user_auth_data(user_id, login, pwd_salt, pwd_hash)
-    return user_id
 
 
-def sign_in(login: str, password: str) -> tuple[bool, int]:
+def sign_in(login: str, password: str, telegram_id: int) -> tuple[bool, int]:
     """
     Авторизация пользователя
+    :param telegram_id: id пользователя в telegram, для определения пользователя,
+    под которым авторизован клиент в системе
     :return:
     При верных данных возвращается [True, user_id], при неверных [False, 0]
     """
     pwd = auth.get_password(login)
     check = is_correct_password(pwd[0], pwd[1], password)
-    return (True, auth.get_user_id(login)) if check else (False, 0)
+    if not check:
+        return False, 0
+    else:
+        user_id = auth.get_user_id(login)
+        authorized_users[telegram_id] = user_id
+        return True, user_id
+
+
+def sign_out(telegram_id: int):
+    """
+    Выход из системы
+    :return Успех операции: True - вышел успешно, False - не был авторизован
+    """
+    if telegram_id in authorized_users:
+        authorized_users.pop(telegram_id)
+        return True
+    else:
+        return False
+
+
