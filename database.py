f1d2c4c166131b722e0d66ca23c7ccff420f5c4c
Maxim <machsim26@gmail.com>
2022-07-06 19:20:07 +0500

    Добавлено получение и установка глобальных параметров

--- database.py
+++ database.py
@@ -5,3 +5,4 @@ from database_interaction import video as video
 from database_interaction import favorite as favorite
 from database_interaction import history as history
 from database_interaction import promocode as promocode
+from database_interaction import global_settings as global_settings
