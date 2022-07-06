17e9482bfa9b3b313803e047c6ba39c89406d123
Maxim <machsim26@gmail.com>
2022-07-06 20:06:47 +0500

    do auth methods

--- authorization.py
+++ authorization.py
@@ -1,2 +1,2 @@
-from authorization_process.auth import sign_in, sign_up, sign_out
+from authorization_process.auth import sign_in, sign_up, sign_out, check_auth, get_authed_user_id
 from database_interaction.auth import get_login_exists
