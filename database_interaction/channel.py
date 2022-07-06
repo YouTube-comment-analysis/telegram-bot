1726ce3be30d179b2b785ce28359359ee4b4ab53
Maxim <machsim26@gmail.com>
2022-07-06 13:58:38 +0500

    Добавлен метод получения видео с канала

--- database_interaction/channel.py
+++ database_interaction/channel.py
@@ -10,3 +10,15 @@ INSERT INTO public.channel
     (channel_url)
     VALUES (%s)
             """, (channel_id,))
+
+
+def get_channel_videos(channel_id: str) -> [str]:
+    """Получить все url_id на загруженные видео с канала"""
+    with connection as connect:
+        with connect.cursor() as curs:
+            curs.execute("""
+SELECT url
+    FROM public.video
+    WHERE channel_url = %s
+            """, (channel_id,))
+            return map(lambda x: x[0], curs)
